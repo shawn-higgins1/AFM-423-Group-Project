ece3925f79256308efb77972f93bd386e8f5738f
shawn-higgins1 <s8higgin@edu.uwaterloo.ca>
2021-02-20 14:02:30 -0500

    Add plots

--- preproccessing/black_scholes.py
+++ preproccessing/black_scholes.py
@@ -2,10 +2,31 @@ import numpy as np
 from scipy.stats import norm
 
 
-def black_scholes_with_dividends(share_price, dividend_yield, t, k, r, volatility):
-    M = share_price * np.exp(-dividend_yield * t)
-    d1 = (np.log(M / k) + (r + (volatility ** 2) / 2) * t) / (volatility * np.sqrt(t))
+def merton_dividend_adjustment(share_price, dividend_yield, t):
+    return share_price * np.exp(-dividend_yield * t)
 
-    d2 = d1 - volatility * np.sqrt(t)
+
+def delta(M, k, r, volatility, t):
+    return (np.log(M / k) + (r + (volatility ** 2) / 2) * t) / (volatility * np.sqrt(t))
+
+
+def delta2(d1, volatility, t):
+    return d1 - volatility * np.sqrt(t)
+
+
+def black_scholes_puts(share_price, dividend_yield, t, k, r, volatility):
+    M = merton_dividend_adjustment(share_price, dividend_yield, t)
+    d1 = delta(M, k, r, volatility, t)
+
+    d2 = delta2(d1, volatility, t)
 
     return k * np.exp(-r * t) * norm.cdf(-d2) - M * norm.cdf(-d1)
+
+
+def black_scholes_calls(share_price, dividend_yield, t, k, r, volatility):
+    M = merton_dividend_adjustment(share_price, dividend_yield, t)
+    d1 = delta(M, k, r, volatility, t)
+
+    d2 = delta2(d1, volatility, t)
+
+    return M * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
--- preproccessing/process_historical_data.py
+++ preproccessing/process_historical_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
-from black_scholes import black_scholes_with_dividends
+from black_scholes import *
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import scipy.stats as ss
 
 # Parent directory where all the csv files are stored
 DATA_DIR = "../data"
@@ -25,6 +28,39 @@ march_option_prices["Expiry Date"] = pd.to_datetime(march_option_prices["Expiry 
 # Load in the historical prices for the S&P TSX 60
 sp_60_prices = pd.read_excel(DATA_DIR + '/PerformanceGraphExport.xls')
 
+
+# Plot the index price data
+plt.plot(
+    sp_60_prices[['Effective date']],
+    sp_60_prices[['S&P/TSX 60 Index']]
+)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('S&P/TSX 60 Index')
+plt.show()
+
+# Extract the volatility information for the index and then plot it
+volatility = sp_60_prices[['Effective date', 'Volatility']]
+volatility = volatility.dropna()
+
+data = volatility.Volatility
+
+# Plot the distribution we'll use to model the data
+x = np.linspace(0, 0.4)
+plt.plot(x, ss.lognorm.pdf(x, 0.75, 0, 0.08))
+plt.title("Log normal 0.5, 0, 0.1 distribution")
+plt.show()
+
+fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+data.hist(ax=axs[0], weights=np.zeros_like(data) + 1. / data.size)
+axs[0].set_title("Volatility Histogram")
+axs[0].set_xlabel('Volatility')
+axs[1].plot(volatility[['Effective date']], volatility[['Volatility']])
+plt.title('S&P/TSX 60 Volatility')
+plt.ylabel('Volatility')
+plt.xlabel('Date')
+plt.show()
+
 # Join the option prices and the index prices
 march_option_prices = pd.merge(march_option_prices, sp_60_prices, left_on='Date', right_on='Effective date')
 
@@ -38,6 +74,19 @@ march_option_prices = march_option_prices.drop(
 # Load in the data for the annual dividend yield for the S&P TSX 60
 dividend_yield = pd.read_excel(DATA_DIR + '/Dividend Yield.xlsx')
 dividend_yield = dividend_yield.rename(columns={'Value': 'Dividend Yield'})
+dividend_yield = dividend_yield.sort_values(by='Date')
+
+# Plot the dividend data
+plt.plot(
+    dividend_yield[['Date']],
+    dividend_yield[['Dividend Yield']]
+)
+plt.title("S&P/TSX 60 Dividend Yield")
+plt.xlabel("Month (2020)")
+plt.ylabel("Annual Dividend Yield")
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+plt.show()
 
 march_option_prices = pd.merge(march_option_prices, dividend_yield, on='Date', how='left')
 
@@ -46,11 +95,32 @@ march_option_prices = pd.merge(march_option_prices, dividend_yield, on='Date', h
 interest_rates = pd.read_csv(DATA_DIR + '/1-month-treasury-rates.csv')
 interest_rates["Date"] = pd.to_datetime(interest_rates["Date"])
 
+# Plot the risk free interest rate data
+plt.plot(
+    interest_rates[['Date']],
+    interest_rates[['Interest Rate']]
+)
+plt.title("1 Month Treasury Rates")
+plt.xlabel("Month (2020)")
+plt.ylabel("Continuously Compounded Interest Rate")
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+plt.show()
+
+
 # Calculate the t value for the options ( the amount of time in years until the option expires)
 march_option_prices = pd.merge(march_option_prices, interest_rates, on='Date', how='left')
 march_option_prices["Time Delta"] = (march_option_prices["Expiry Date"] - march_option_prices["Date"])
 march_option_prices["t"] = march_option_prices["Time Delta"].dt.days / 366
 march_option_prices = march_option_prices.drop(["Time Delta", "Expiry Date"], axis=1)
+
+# Plot the time to expiry data
+data = march_option_prices[['t']]
+fig, axs = plt.subplots(1, 1, figsize=(20, 10))
+data.hist(ax=axs, weights=np.zeros_like(data) + 1. / data.size)
+axs.set_title("Time to Expiry Histogram")
+axs.set_xlabel('Time to Expiry')
+plt.show()
 
 # Split the options into calls and puts
 call_options = march_option_prices[march_option_prices["Call/Put"] == 0]
@@ -59,7 +129,7 @@ call_options['Black Scholes'] = 0.0
 
 # Calculate the black scholes price for the calls
 for i, call_option in call_options.iterrows():
-    call_options.at[i, 'Black Scholes'] = black_scholes_with_dividends(
+    call_options.at[i, 'Black Scholes'] = black_scholes_calls(
         call_option['Index Price'],
         call_option['Dividend Yield'],
         call_option['t'],
@@ -77,7 +147,7 @@ put_options['Black Scholes'] = 0.0
 
 # Calculate the black scholes price for the puts
 for i, put_option in put_options.iterrows():
-    put_options.at[i, 'Black Scholes'] = black_scholes_with_dividends(
+    put_options.at[i, 'Black Scholes'] = black_scholes_puts(
         put_option['Index Price'],
         put_option['Dividend Yield'],
         put_option['t'],
