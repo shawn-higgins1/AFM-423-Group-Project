83dca619cd5f192332da4cfba35671dc2f7e8d7c
shawn-higgins1 <s8higgin@edu.uwaterloo.ca>
2021-03-02 18:20:58 -0500

    Enable MNNs

--- models/models.py
+++ models/models.py
@@ -16,9 +16,6 @@ import kerastuner as kt
 
 tf.get_logger().setLevel('ERROR')
 
-#  Specify a seed if you want reproducibility
-tf.random.set_seed(123)
-
 # Make's for easier debugging / printing when displaying numpy arrays
 np.set_printoptions(precision=3, suppress=True)
 
@@ -91,102 +88,119 @@ def model_builder_2_layer(hp):
     return model
 
 
-def train_and_test_model(model_type, option_type, train_features, train_labels, test1_features, test1_labels,
-                         test2_features, test2_labels):
+def train_and_test_model(model_type, option_type, train_features, train_labels, test_generated_features, test_generated_labels,
+                         test_real_features, test_real_labels):
     stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 
-    model_checkpoint_dir = "./" + option_type
-    model_log_dir = LOG_DIR + "/" + option_type
+    models = []
 
-    tuner_1_layer = kt.Hyperband(model_builder_1_layer,
-                                 objective='val_loss',
-                                 max_epochs=15,
-                                 factor=3,
-                                 directory=model_checkpoint_dir + "/" + model_type,
-                                 project_name='1_layer')
+    model_checkpoint_dir = f"./{option_type}"
+    model_log_dir = LOG_DIR + f"./{option_type}"
 
-    tuner_1_layer.search(train_features, train_labels, epochs=50, validation_split=VALIDATION_SPLIT,
-                         callbacks=[stop_early,
-                                    tf.keras.callbacks.TensorBoard(model_log_dir + f"/{model_type}/tuning/1_layer")],
-                         verbose=PRINT_TENSORFLOW_PROGRESS)
+    for i in range(len(train_labels)):
+        print(f"Generating {option_type} {model_type} model {i}")
 
-    best_hps_1_layer = tuner_1_layer.get_best_hyperparameters(num_trials=1)[0]
+        tuner_1_layer = kt.Hyperband(model_builder_1_layer,
+                                     objective='val_loss',
+                                     max_epochs=15,
+                                     factor=3,
+                                     directory=model_checkpoint_dir + f"/{model_type}_{i}",
+                                     project_name='1_layer')
 
-    print(f"""
-        1 layer search complete. Optimal first layer units: {best_hps_1_layer.get('units')}, optimal learning rate: 
-        {best_hps_1_layer.get('learning_rate')}.
-        """)
+        tuner_1_layer.search(train_features[i], train_labels[i], epochs=50, validation_split=VALIDATION_SPLIT,
+                             callbacks=[stop_early,
+                                        tf.keras.callbacks.TensorBoard(
+                                            model_log_dir + f"/{model_type}_{i}/tuning/1_layer"
+                                        )],
+                             verbose=PRINT_TENSORFLOW_PROGRESS)
 
-    tuner_2_layer = kt.Hyperband(model_builder_2_layer,
-                                 objective='val_loss',
-                                 max_epochs=15,
-                                 factor=3,
-                                 directory=model_checkpoint_dir + "/" + model_type,
-                                 project_name='2_layer')
+        best_hps_1_layer = tuner_1_layer.get_best_hyperparameters(num_trials=1)[0]
 
-    tuner_2_layer.search(train_features, train_labels, epochs=50, validation_split=VALIDATION_SPLIT,
-                         callbacks=[stop_early,
-                                    tf.keras.callbacks.TensorBoard(model_log_dir + f"/{model_type}/tuning/2_layer")],
-                         verbose=PRINT_TENSORFLOW_PROGRESS)
+        print(f"""
+            1 layer search complete. Optimal first layer units: {best_hps_1_layer.get('units')}, optimal learning rate: 
+            {best_hps_1_layer.get('learning_rate')}.
+            """)
 
-    best_hps_2_layer = tuner_2_layer.get_best_hyperparameters(num_trials=1)[0]
+        tuner_2_layer = kt.Hyperband(model_builder_2_layer,
+                                     objective='val_loss',
+                                     max_epochs=15,
+                                     factor=3,
+                                     directory=model_checkpoint_dir + f"/{model_type}_{i}",
+                                     project_name='2_layer')
 
-    print(f"""
-        2 layer optimized search complete. Optimal first layer units: {best_hps_2_layer.get('first_units')}, second: 
-        {best_hps_2_layer.get('second_units')}, optimal learning rate: {best_hps_2_layer.get('learning_rate')}.
-        """)
+        tuner_2_layer.search(train_features[i], train_labels[i], epochs=50, validation_split=VALIDATION_SPLIT,
+                             callbacks=[stop_early,
+                                        tf.keras.callbacks.TensorBoard(
+                                            model_log_dir + f"/{model_type}_{i}/tuning/2_layer")
+                                        ],
+                             verbose=PRINT_TENSORFLOW_PROGRESS)
 
-    save_path = f'saved_model/{option_type}_{model_type}'
+        best_hps_2_layer = tuner_2_layer.get_best_hyperparameters(num_trials=1)[0]
 
-    if not os.path.exists(save_path) or len(os.listdir(save_path)) == 0:
-        layer1_model = tuner_1_layer.hypermodel.build(best_hps_1_layer)
-        layer2_model = tuner_2_layer.hypermodel.build(best_hps_2_layer)
+        print(f"""
+            2 layer optimized search complete. Optimal first layer units: {best_hps_2_layer.get('first_units')}, second: 
+            {best_hps_2_layer.get('second_units')}, optimal learning rate: {best_hps_2_layer.get('learning_rate')}.
+            """)
 
-        start_time = perf_counter()
-        history_1_layer = layer1_model.fit(train_features, train_labels, epochs=50, validation_split=VALIDATION_SPLIT,
-                                           callbacks=[stop_early], verbose=PRINT_TENSORFLOW_PROGRESS)
-        print("Elapsed time:", perf_counter() - start_time)
+        save_path = f'saved_model/{option_type}_{model_type}_{i}'
 
-        start_time = perf_counter()
-        history_2_layer = layer2_model.fit(train_features, train_labels, epochs=50, validation_split=VALIDATION_SPLIT,
-                                           callbacks=[stop_early], verbose=PRINT_TENSORFLOW_PROGRESS)
-        print("Elapsed time:", perf_counter() - start_time)
+        if not os.path.exists(save_path) or len(os.listdir(save_path)) == 0:
+            layer1_model = tuner_1_layer.hypermodel.build(best_hps_1_layer)
+            layer2_model = tuner_2_layer.hypermodel.build(best_hps_2_layer)
 
-        if history_2_layer.history['val_loss'] > history_1_layer.history['val_loss']:
-            history = history_1_layer
-            hypermodel = tuner_1_layer.hypermodel.build(best_hps_1_layer)
-            print("The 1 layer model had the lowest loss")
+            start_time = perf_counter()
+            history_1_layer = layer1_model.fit(train_features[i], train_labels[i], epochs=50,
+                                               validation_split=VALIDATION_SPLIT,
+                                               callbacks=[stop_early], verbose=PRINT_TENSORFLOW_PROGRESS)
+            print("Elapsed time:", perf_counter() - start_time)
+
+            start_time = perf_counter()
+            history_2_layer = layer2_model.fit(train_features[i], train_labels[i], epochs=50,
+                                               validation_split=VALIDATION_SPLIT,
+                                               callbacks=[stop_early], verbose=PRINT_TENSORFLOW_PROGRESS)
+            print("Elapsed time:", perf_counter() - start_time)
+
+            if history_2_layer.history['val_loss'] > history_1_layer.history['val_loss']:
+                history = history_1_layer
+                hypermodel = tuner_1_layer.hypermodel.build(best_hps_1_layer)
+                print("The 1 layer model had the lowest loss")
+            else:
+                history = history_2_layer
+                hypermodel = tuner_2_layer.hypermodel.build(best_hps_2_layer)
+                print("The 2 layer model had the lowest loss")
+
+            if PLOT_INDIVIDUAL_LOSSES:
+                plot_loss(history)
+
+            val_loss_per_epoch = history.history['val_loss']
+            best_epoch = val_loss_per_epoch.index(min(val_loss_per_epoch)) + 1
+            print('Best epoch: %d' % (best_epoch,))
+
+            # Retrain the model
+            start_time = perf_counter()
+            hypermodel.fit(train_features[i], train_labels[i], epochs=best_epoch,
+                           callbacks=[tf.keras.callbacks.TensorBoard(model_log_dir + f"/{model_type}_{i}/best")],
+                           verbose=PRINT_TENSORFLOW_PROGRESS)
+            print("Elapsed time:", perf_counter() - start_time)
+
+            hypermodel.save(save_path)
+            models.append(hypermodel)
         else:
-            history = history_2_layer
-            hypermodel = tuner_2_layer.hypermodel.build(best_hps_2_layer)
-            print("The 2 layer model had the lowest loss")
+            models.append(tf.keras.models.load_model(save_path, custom_objects={'MyRSquared': MyRSquared}))
 
-        if PLOT_INDIVIDUAL_LOSSES:
-            plot_loss(history)
+    y_pred = np.array([])
+    y_true = np.array([])
 
-        val_loss_per_epoch = history.history['val_loss']
-        best_epoch = val_loss_per_epoch.index(min(val_loss_per_epoch)) + 1
-        print('Best epoch: %d' % (best_epoch,))
-
-        # Retrain the model
-        start_time = perf_counter()
-        hypermodel.fit(train_features, train_labels, epochs=best_epoch,
-                       callbacks=[tf.keras.callbacks.TensorBoard(model_log_dir + f"/{model_type}/best")],
-                       verbose=PRINT_TENSORFLOW_PROGRESS)
-        print("Elapsed time:", perf_counter() - start_time)
-
-        hypermodel.save(save_path)
-    else:
-        hypermodel = tf.keras.models.load_model(save_path, custom_objects={'MyRSquared': MyRSquared})
-
-    test_results = hypermodel.evaluate(test1_features, test1_labels, verbose=PRINT_TENSORFLOW_PROGRESS)
-
-    test_predictions = hypermodel.predict(test1_features, verbose=PRINT_TENSORFLOW_PROGRESS).flatten()
+    for i in range(len(models)):
+        test = models[i].predict(test_generated_features[i],
+                                 verbose=PRINT_TENSORFLOW_PROGRESS).flatten()
+        y_pred = np.concatenate((y_pred, test))
+        y_true = np.concatenate((y_true, test_generated_labels[i].to_numpy()))
 
     percentage_errors = []
 
-    for i in range(len(test_predictions)):
-        percentage_errors.append(np.abs(test1_labels[i] - test_predictions[i]) / test1_labels[i])
+    for i in range(len(y_pred)):
+        percentage_errors.append(np.abs(y_pred[i] - y_true[i]) / y_true[i])
 
     plt.hist(percentage_errors, bins=25)
     plt.xlabel('Percentage Errors [C/K]')
@@ -196,7 +210,7 @@ def train_and_test_model(model_type, option_type, train_features, train_labels, 
     print("Median Absolute Percentage Error: " + str(statistics.median(percentage_errors)))
 
     a = plt.axes(aspect='equal')
-    plt.scatter(test1_labels, test_predictions)
+    plt.scatter(y_true, y_pred)
     plt.xlabel('True Values [C/K]')
     plt.ylabel('Predictions [C/K]')
     lims = [0, 0.5]
@@ -205,33 +219,51 @@ def train_and_test_model(model_type, option_type, train_features, train_labels, 
     _ = plt.plot(lims, lims)
     plt.show()
 
-    error = test_predictions - test1_labels
+    error = y_pred - y_true
     plt.hist(error, bins=25)
     plt.xlabel('Prediction Error [C/K]')
     _ = plt.ylabel('Count')
     plt.show()
 
-    hist = pd.DataFrame(test_results).T
-    hist = hist.rename(columns={0: "MSE", 1: "MAE", 2: "MAPE", 3: "R^2"})
-    print(hist.tail())
+    mape = tf.keras.losses.MeanAbsolutePercentageError()
+    mae = tf.keras.losses.MeanAbsoluteError()
+    mse = tf.keras.losses.MeanSquaredError()
+    r_squared = tfa.metrics.RSquare()
 
-    if len(test2_labels) > 0:
+    print(f"""
+    mape: {mape(y_true, y_pred).numpy()}, mse: {mse(y_true, y_pred).numpy()}, mae: {mae(y_true, y_pred).numpy()}, 
+    r^2: {r_squared(y_true, y_pred).numpy()}
+    """)
+
+    y_pred = np.array([])
+    y_true = np.array([])
+
+    for i in range(len(models)):
+        if len(test_real_labels[i]) > 0:
+            y_pred = np.concatenate((y_pred,
+                                     models[i].predict(test_real_features[i],
+                                                       verbose=PRINT_TENSORFLOW_PROGRESS).flatten()), axis=0)
+            y_true = np.concatenate((y_true, test_real_labels[i].to_numpy()))
+
+    if len(y_true) > 0:
         print("Performance against the real test data:")
-
-        test_results = hypermodel.evaluate(test2_features, test2_labels, verbose=PRINT_TENSORFLOW_PROGRESS)
-
-        test_predictions = hypermodel.predict(test2_features, verbose=PRINT_TENSORFLOW_PROGRESS).flatten()
 
         percentage_errors = []
 
-        for i in range(len(test_predictions)):
-            percentage_errors.append(np.abs(test1_labels[i] - test_predictions[i]) / test1_labels[i])
+        for i in range(len(y_pred)):
+            percentage_errors.append(np.abs(y_true[i] - y_pred[i]) / y_pred[i])
 
         print("Median Absolute Percentage Error: " + str(statistics.median(percentage_errors)))
 
-        hist = pd.DataFrame(test_results).T
-        hist = hist.rename(columns={0: "MSE", 1: "MAE", 2: "MAPE", 3: "R^2"})
-        print(hist.tail())
+        mape = tf.keras.losses.MeanAbsolutePercentageError()
+        mae = tf.keras.losses.MeanAbsoluteError()
+        mse = tf.keras.losses.MeanSquaredError()
+        r_squared = tfa.metrics.RSquare()
+
+        print(f"""
+            mape: {mape(y_true, y_pred).numpy()}, mse: {mse(y_true, y_pred).numpy()}, mae: {mae(y_true, y_pred).numpy()}, 
+            r^2: {r_squared(y_true, y_pred).numpy()}
+            """)
 
 
 def train_and_test_all_models(option_type):
@@ -240,20 +272,22 @@ def train_and_test_all_models(option_type):
     raw_test1 = pd.read_csv(DATA_DIR + f"/test_{option_type}.csv", sep=',')
     raw_test2 = pd.read_csv(DATA_DIR + f"/{option_type}.csv", sep=',')
 
+    features = ['S/K', 't', 'D', 'r', 'sigma']
+
     train_dataset = raw_train.copy()
     test1_dataset = raw_test1.copy()
     test2_dataset = raw_test2.copy()
 
-    train_features = train_dataset[['S/K', 't', 'D', 'r', 'sigma']]
-    test1_features = test1_dataset[['S/K', 't', 'D', 'r', 'sigma']]
-    test2_features = test2_dataset[['S/K', 't', 'D', 'r', 'sigma']]
+    train_features = train_dataset[features]
+    test1_features = test1_dataset[features]
+    test2_features = test2_dataset[features]
 
     train_labels = train_dataset['C/K']
     test1_labels = test1_dataset['C/K']
     test2_labels = test2_dataset['C/K']
 
-    train_and_test_model("ANN", option_type, train_features, train_labels, test1_features, test1_labels,
-                         test2_features, test2_labels)
+    train_and_test_model("ANN", option_type, [train_features], [train_labels], [test1_features], [test1_labels],
+                         [test2_features], [test2_labels])
 
 
 print("Black Scholes Performance on the real test data for puts:")
